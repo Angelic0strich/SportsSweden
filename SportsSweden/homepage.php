090ac5ac03bac917766cd8b0d5f5aeb4b7ad7239
Angelic0strich <julien.savornin@gmail.com>
2019-10-16 11:22:54 +0200

    Welcome

--- SportsSweden/homepage.php
+++ SportsSweden/homepage.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Sports Sweden</title>
+    <link rel="icon" type="image/png" href="img/Sports-Sweden-crop-logo.png">
 </head>
 <body>
+<h1>Welcome</h1>
+<img src="img/Sports-Sweden-full-logo.png">
 
 </body>
 </html>

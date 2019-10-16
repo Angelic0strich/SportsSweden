d2e83cb983271d67fb9ef1ae2e39b8e83f9f9e1c
Angelic0strich <julien.savornin@gmail.com>
2019-10-16 11:36:24 +0200

    add stylesheet

--- html/homepage.php
+++ html/homepage.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Sports Sweden</title>
-    <link rel="icon" type="image/png" href="img/Sports-Sweden-crop-logo.png">
+    <link rel="icon" type="image/png" href="../img/Sports-Sweden-crop-logo.png">
+    <link rel="stylesheet" href="../css/homepage.css">
 </head>
 <body>
 <h1>Welcome</h1>
-<img src="img/Sports-Sweden-full-logo.png">
+<img src="../img/Sports-Sweden-full-logo.png">
 
 </body>
 </html>

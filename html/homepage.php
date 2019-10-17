56c2a760c1c82cce9b967ff8dfe87d64425764ee
Angelic0strich <julien.savornin@gmail.com>
2019-10-17 23:01:23 +0200

    modification on menu

--- html/homepage.php
+++ html/homepage.php
@@ -5,6 +5,9 @@
     <title>Sports Sweden</title>
     <link rel="icon" type="image/png" href="../img/Sports-Sweden-crop-logo.png">
     <link rel="stylesheet" href="../css/homepage.css">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Patua+One&display=swap" rel="stylesheet">
+
 </head>
 <body>
 <?php include ("menu.php")?>

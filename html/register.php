3e2ddbf65058c1b796a13415076e144b53e507ee
altaberg <joakim.altaberg@gmail.com>
2019-10-23 10:51:14 +0200

    test, start of register

---
+++ html/register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form action="register.php" method="post">
+        User name: <input type="text" name="user"><br>
+        Password: <input type="text" name="password"><br>
+        Re-enter password: <input type="text" name="password2"><br>
+        First name: <input type="text" name="firstname"><br>
+        Last name: <input type="text" name="lastname"><br>
+        E-mail address: <input type="text" name="email"><br>
+
+
+        <input type="submit" value="Reset"> <!-- Do we need a reset? -->
+        <input type="submit" value="Submit" onclick="">
+    </form>
+</body>
+</html>

c986a1c434cb2a2656b11b851e53a01ce1e76a24
Angelic0strich <julien.savornin@gmail.com>
2019-10-23 13:14:33 +0200

    homepage

--- html/homepage.php
+++ html/homepage.php
@@ -13,16 +13,39 @@
 <?php include ("menu.php")?>
 <div class = "content">
     <div class = "welcoming">
-        <h1>Welcome</h1>
-        <h3>Lorem ipsum bla bla bla</h3>
+        <h1>Greetings, visitor !</h1>
+        <h3>And welcome in the Sports Sweden platform !</h3>
+        <p><strong>Here you will find news about sport clubs in Sweden main cities, discover new clubs around your city, and register in it !</strong></p>
     </div>
     <div class="news-panel">
-        <div class="news-one"></div>
-        <div class="news-two"></div>
-        <div class="news-three"></div>
-        <div class="news-four"></div>
-        <div class="news-five"></div>
-        <div class="news-six"></div>
+        <div class="news-one">
+            <img src="../img/image_2.jpg">
+            <span class="content">
+					floorball team are ready to world cup
+            </span>
+        </div>
+        <div class="news-two">
+            &nbsp;
+           <!-- <img src="../img/hoki.jpg">
+            <span class="content">
+					Sweden team prepareing for world cup
+            </span>-->
+        </div>
+        <div class="news-three">
+            <!--<img src="../img/th.jpg">
+            <span class="content">
+					New Racing club
+				</span>-->
+        </div>
+        <div class="news-four">
+
+        </div>
+        <div class="news-five">
+
+        </div>
+        <div class="news-six">
+
+        </div>
 
     </div>
 

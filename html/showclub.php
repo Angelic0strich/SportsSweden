a65c0fe60a055d07eea05da1d40e107d9d59792f
Angelic0strich <julien.savornin@gmail.com>
2019-11-05 23:43:24 +0100

    idk what is life anymore

--- html/showclub.php
+++ html/showclub.php
@@ -43,12 +43,12 @@ else
     <div class="container">
         <tr>
             <br>
-            <td><img src="<?php echo $row->image;?>" ></td>
+            <td><img class="image" src="<?php echo $row->image;?>" ></td>
             <td><h1 style="text-align: center"><?php echo $row->name ?></h1></td>
             <td><h2 style="text-align: center">Sport : <?php echo $row->sport ?></h2></td>
 
         </tr>
-        <img src="<?php echo $row->logo ?>" style="display: block; margin: 0 auto; max-width: 100%;">
+        <img class="logo" src="<?php echo $row->logo ?>" style="display: block; margin: 0 auto; max-width: 100%;">
         <br>
 
         <br>

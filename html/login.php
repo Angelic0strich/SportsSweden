8d604069d789cd352c1326eb5f2c6f77c683772b
Edvin <ediparmeza94@yahoo.com>
2020-01-20 20:36:37 +0100

    hey

--- html/login.php
+++ html/login.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 require_once "../php/imports.php";
 require_once "../php/password_check_user.php";
@@ -16,3 +17,209 @@ if (isset($_POST['submit']) && isset($_POST['uname']) && isset($_POST['psw'])) {
     setLoginFailed();
 }
 header("Location: index.php");
+=======
+
+
+
+
+<html>
+<head>
+    <title>Log In</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="../img/Sports-Sweden-crop-logo.png">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Patua+One&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: "Patua One", sans-serif;
+            background-color: white;
+            color: #00c2e5;
+        }
+
+        /* Full-width input fields */
+
+
+        input[type=text], input[type=password] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+        }
+
+        /* Set a style for all buttons */
+        button {
+            font-family: "Fjalla One", "Arial Black";
+            font-size: 20px;
+            background-color: #00c2e5;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            cursor: pointer;
+            width: 100%;
+        }
+
+        button:hover {
+            opacity: 0.8;
+        }
+
+        /* Extra styles for the cancel button */
+        .cancelbtn {
+            width: auto;
+            padding: 10px 18px;
+            background-color: #f44336;
+        }
+
+        /* Center the image and position the close button */
+        .imgcontainer {
+            text-align: center;
+            margin: 24px 0 12px 0;
+            position: relative;
+        }
+
+        img.profile {
+            width: 15%;
+            border-radius: 20%;
+            background-color: #3b3333;
+        }
+
+        .container {
+            padding: 16px;
+        }
+
+        span.psw {
+            float: right;
+            padding-top: 16px;
+        }
+
+        /* The Modal (background) */
+        .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgba(0, 0, 0, 0.4); /* Fallback color */
+            background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+            padding-top: 60px;
+        }
+
+        /* Modal Content/Box */
+        .modal-content {
+            background-color: #3b3333;
+            margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+            border: 1px solid #888;
+            width: 80%; /* Could be more or less, depending on screen size */
+        }
+
+        /* The Close Button (x) */
+        .close {
+            position: absolute;
+            right: 25px;
+            top: 0;
+            color: #000;
+            font-size: 35px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: red;
+            cursor: pointer;
+        }
+
+        /* Add Zoom Animation */
+        .animate {
+            -webkit-animation: animatezoom 0.6s;
+            animation: animatezoom 0.6s
+        }
+
+        @-webkit-keyframes animatezoom {
+            from {
+                -webkit-transform: scale(0)
+            }
+            to {
+                -webkit-transform: scale(1)
+            }
+        }
+
+        @keyframes animatezoom {
+            from {
+                transform: scale(0)
+            }
+            to {
+                transform: scale(1)
+            }
+        }
+
+        /* Change styles for span and cancel button on extra small screens */
+        @media screen and (max-width: 300px) {
+            span.psw {
+                display: block;
+                float: none;
+            }
+
+            .cancelbtn {
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+<body>
+
+
+<h1>LOGIN PAGE</h1>
+
+
+
+    <button onclick="location.href = 'homepage.php';" style="width:auto;">Home page</button>
+    <button onclick="document.getElementById('edvin').style.display='block'" style="width:auto;">Sign In As Admin
+    </button>
+
+
+<div id="edvin" class="modal">
+
+    <form class="modal-content animate" method="post" action='#'>
+
+        <h1>Admin</h1>
+
+        <div class="imgcontainer">
+            <span onclick="document.getElementById('edvin').style.display='none'" class="close"
+                  title="Close Box">&times;</span>
+            <img src="../img/profile.jpg" alt="Profile" class="profile">
+        </div>
+        <div class="container">
+            <label for="em"><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="em" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <button type="submit" name="submit">Login</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+            <button type="button" onclick="document.getElementById('edvin').style.display='none'" class="cancelbtn">
+                Cancel
+            </button>
+            <span class="psw"><a href="/sportsweden/html/register_admin.php">Register</a></span>
+
+        </div>
+    </form>
+</div>
+
+
+
+
+</body>
+</html>
+
+>>>>>>> hey
--- html/menu.php
+++ html/menu.php
@@ -1,7 +1,43 @@
+<<<<<<< HEAD
+=======
+<?php
+require_once "../php/imports.php";
+require "../php/password_check_user.php";
+require "../php/password_check_admin.php";
+
+if (isset($_SESSION['role']) && $_SESSION['role'] === 'user') {
+    header("Location: user.php");
+} else if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') {
+    header("Location: addnews.php");
+}
+
+if (isset($_POST['submit'])) {
+    if (check_password_admin($_POST['em'], $_POST['psw'])) {
+        $_SESSION['role'] = 'admin';
+        $_SESSION['email'] = $_POST['em'];
+        header("Location: addnews.php");
+    } else if (check_password($_POST['uname'], $_POST['psw'])) {
+        $_SESSION['role'] = "user";
+        $_SESSION['username'] = $_POST['uname'];
+        header("Location: category.php");
+    } else {
+        echo "<h1>Wrong username or password</h1>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/menu.css">
+</head>
+>>>>>>> hey
 <nav id="menu">
 
     <div id="menu-bar">
         <!-- LOGIN -->
+<<<<<<< HEAD
         <?php
         //require "../php/loggedInStatus.php";
         if(isUserLoggedIn()){
@@ -11,6 +47,11 @@
             echo '<div class="login" onclick="show_login_popup()"><a href="#">Login</a></div>';
         }
         ?>
+=======
+        <div class="container edvin-content">
+        <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+        </div>>
+>>>>>>> hey
         <div id="search-container"><input id="search-data" type="text" placeholder="Search"/>
             <div id="search-result-container"></div>
         </div>
@@ -50,6 +91,38 @@
         }
     );
 
+    <div id="id01" class="modal">
+
+        <form class="modal-content animate" method="post" action='#'>
+        <h1>User</h1>
+        <div class="imgcontainer">
+        <span onclick="document.getElementById('id01').style.display='none'" class="close"
+    title="Close Box">&times;</span>
+    <img src="../img/profile.jpg" alt="Profile" class="profile">
+        </div>
+
+        <div class="container">
+        <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit" name="submit">Login</button>
+        <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+    </div>
+    <div class="container" style="background-color:#f1f1f1">
+        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">
+        Cancel
+        </button>
+
+        <span class="psw"><a href="/sportsweden/html/register_user.php">Register</a></span>
+
+    </div>
+    </form>
+    </div>
 
 </script>
 </nav>
